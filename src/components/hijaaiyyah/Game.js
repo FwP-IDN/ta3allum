890e696aea8bd9950e44e6b3de911b2105744412
Febriananda Wida Pramudita <febriwpramu@gmail.com>
2025-04-27 23:15:05 +0700

    add progress

--- src/components/hijaaiyyah/Game.js
+++ src/components/hijaaiyyah/Game.js
@@ -1,17 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { 
+  // useState,
+  useEffect } from "react";
 import PropTypes from "prop-types";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Alert from "@material-ui/lab/Alert";
 import "./Game.css";
-import { Button } from "@material-ui/core";
-import Snackbar from "@material-ui/core/Snackbar";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogActions from "@material-ui/core/DialogActions";
-import Typography from "@material-ui/core/Typography";
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -19,12 +13,10 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
 
 
-import hijaaiyyah from "../../data/hijaaiyyah";
+// import hijaaiyyah from "../../data/hijaaiyyah";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -54,16 +46,16 @@ const useStyles = makeStyles({
   },
 });
 
-const answerStatusEnum = {
-  CORRECT: "✅",
-  WRONG: "❌",
-  EMPTY: "⚪",
-};
+// const answerStatusEnum = {
+//   CORRECT: "✅",
+//   WRONG: "❌",
+//   EMPTY: "⚪",
+// };
 
-const LocalStatusEnum = {
-  ONGOING: "ONGOING",
-  FINISHED: "FINISHED",
-};
+// const LocalStatusEnum = {
+//   ONGOING: "ONGOING",
+//   FINISHED: "FINISHED",
+// };
 
 
 const Game = (props) => {
@@ -74,8 +66,8 @@ const Game = (props) => {
 
   const classes = useStyles();
 
-  const [status, setStatus] = useState(LocalStatusEnum.ONGOING);
-  const [showErrorSnackbar, setShowErrorSnackbar] = useState(false);
+  // const [status, setStatus] = useState(LocalStatusEnum.ONGOING);
+  // const [showErrorSnackbar, setShowErrorSnackbar] = useState(false);
 
   useEffect(() => {}, []);
 
